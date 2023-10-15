bb459a7619b97ad6414f519cbda96bb12dcf70fc
fabri <fguespe@gmail.com>
2023-10-15 17:29:26 -0300

    init

---
+++ theme.config.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>XMTP Prototypes</span>,
+  project: {
+    link: "https://github.com/xmtp",
+  },
+
+  docsRepositoryBase: "https://github.com/xmtp",
+  footer: {
+    text: "Nextra Docs Template",
+  },
+};
+
+export default config;
