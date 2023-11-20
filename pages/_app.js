bb11aaf3433cae6929bc8243fd297ec6150aec47
fabri <fguespe@gmail.com>
2023-11-20 18:36:05 -0300

    styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
-// pages/_app.js
-import Head from "next/head";
+import "../styles.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Default Title</title>
-        <meta name="description" content="Default description" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
