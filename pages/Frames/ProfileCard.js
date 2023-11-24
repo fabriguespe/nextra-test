16bd96ab3ddf7eab60b7fa2b59240ae3c7f173c4
fabri <fguespe@gmail.com>
2023-11-24 14:52:58 -0300

    ProfileCard fix

--- pages/Frames/ProfileCard.js
+++ pages/Frames/ProfileCard.js
@@ -1,11 +1,13 @@
-import { ContactPage } from "/components/ContactPage";
+import { ProfileCard } from "/components/ProfileCard";
 
 export default function BlankPage() {
   return (
-    <div className="widget-container Link">
-      <ContactPage
+    <div className="widget-container">
+      <ProfileCard
         domain="shanemac.eth"
         walletAddress="0x7E0b0363404751346930AF92C80D1fef932Cc48a"
+        description="Hi, I'm Shane, Co-Founder of XMTP."
+        image="https://pbs.twimg.com/profile_images/1561559544148500480/lBJtF9DK_400x400.jpg"
       />
     </div>
   );
